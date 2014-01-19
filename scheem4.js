6192420e8fe71d91635c15738544e24c092206c4
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-01-19 19:18:10 +0000

    quote implemented.

--- scheem4.js
+++ scheem4.js
@@ -1,7 +1,19 @@
 /*
-    http://nathansuniversity.com/scheem3.html
+    http://nathansuniversity.com/scheem4.html
 
-    Write a version of evalScheem that can handle addition, set!, and begin.
+    Understanding the distinction between expressions and quoted
+    expressions is important. The expression ['+', 2, 3] evaluates
+    to 5, but the expression ['quote', ['+', 2, 3]] evaluates to
+    ['+', 2, 3]. This is actually a little bit trickier than it
+    seems. It actually means that any valid Scheem expression is
+    also a valid Scheem value; just put a quote in front of it to
+    use it as a value. This means we can assign Scheem expressions
+    to variables, use Scheem expressions as data, and generally rip
+    apart and put back together any Scheem expressions. This is one
+    of the most powerful aspects of languages in the Lisp family;
+    we'll revisit this feature again in the future.
+
+    Write a version of evalScheem that can do addition and quotation.
 */
 
 var evalScheem = function (expr, env) {
